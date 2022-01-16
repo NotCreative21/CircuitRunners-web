9973ae58e8cb6985ceab2b3526c180f953acb8c1
NotCreative21 <seanray410@gmail.com>
2022-01-16 00:53:10 -0500

    several style fixes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../index.css";
-import styles from "../styles/home.module.scss";
+import styles from "../styles/page.module.scss";
 
 export function Home() {
     return (
@@ -10,19 +10,16 @@ export function Home() {
                     <div className={styles.slogan}>
                         FROM <div className={styles.cyan}>POTENTIAL</div> TO <div className={styles.red}>KINETIC</div>
                     </div>
-                    <div className={styles.hidden}>
-                        WE'RE CIRCUITRUNNERS ROBOTICS AND WE DON'T JUST BUILD ROBOTS!
+                    <div className={styles.container}>
+                        <div className={styles.hidden}>
+                            WE'RE CIRCUITRUNNERS ROBOTICS AND WE DON'T JUST BUILD ROBOTS!
+                        </div>
                     </div>
                 </div>
-                <div className={styles.p}>
-                    home
-                    dfsg
-                    dfgh
-                    dfgh
-                    dfgh
-                    dfgh
+                <div className={styles.text}>
+                    To learn more about our club see our <a href="./about">about</a> page.
                 </div>
-                <div className={styles.footer}>
+                <div className="footer">
                     © 2021 CircuitRunners Robotics Inc.
                 </div>
             </center>
